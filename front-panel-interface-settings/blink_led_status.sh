ea4a4e1faec6893c683f057c3571e722073c6b2d
mmiller7 <mmiller7@mail.umw.edu>
2015-08-09 17:09:55 -0400

    Script to poll status and blink LED

---
+++ front-panel-interface-settings/blink_led_status.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# blink_led_status.sh
+# Created by Matthew Miller
+# 08AUG2015
+# Polls status and blinks codes.
+
+# Blink this GPIO LED to send status
+# Uses 1st parameter by default
+BLINK_PIN=$1
+
+# Delay seconds between iterations
+BLINK_DELAY_OFFTIME=1
+
+# seconds on for "long blink" (fail)
+BLINK_DELAY_LONG=0.5
+
+# Seconds on for "short blink" (pass)
+BLINK_DELAY_SHORT=0.15
+
+
+
+# Setup
+if [ "$BLINK_PIN" == "" ]; then
+  echo "blink_led_status.sh: ERROR - GPIO pin not specified!"
+  exit 1
+fi
+echo ${BLINK_PIN} > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio${BLINK_PIN}/direction
+echo "Flashing run indicator on GPIO ${BLINK_PIN}"
+
+blinkOnce()
+{
+  BLINK_DELAY=$1
+  echo 1 > /sys/class/gpio/gpio${BLINK_PIN}/value
+  sleep $BLINK_DELAY
+  echo 0 > /sys/class/gpio/gpio${BLINK_PIN}/value
+  sleep $BLINK_DELAY
+}
+
+# Performs a test and blinks per exit result
+doTest()
+{
+  cmd=$1
+  $cmd &> /dev/null && blinkOnce $BLINK_DELAY_SHORT || blinkOnce $BLINK_DELAY_LONG
+}
+
+# Test loop
+while true
+do
+  # Internet status blink
+  #ping -c 1 -s 0 google.com > /dev/null && blinkOnce $BLINK_DELAY_SHORT || blinkOnce $BLINK_DELAY_LONG
+  doTest "ping -c 1 -s 0 google.com"
+
+  # Svxlink status blink
+  #pgrep svxlink > /dev/null && blinkOnce $BLINK_DELAY_SHORT || blinkOnce $BLINK_DELAY_LONG
+  doTest "pgrep svxlink"
+
+  # Wait to restart loop
+  sleep $BLINK_DELAY_OFFTIME
+done
+     
